de1cdd531630846c6e1a3d325340c6fa815b67a1
esnue <65082334+esnue@users.noreply.github.com>
2020-08-03 13:32:54 +0200

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -36,6 +36,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'})
     ]
 }
